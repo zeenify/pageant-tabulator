8f4e031323260ed2712b887551d4ab3459a86c6d
Edrick Martin <edrickmartin90@gmail.com>
2026-03-26 12:15:48 +0800

    pikachu

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,12 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*'); // 👈 added this
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
